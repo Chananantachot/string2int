efe88ff4fccd92edc37d0d250c47207774da1bdc
atip chananantachot <udth2013@outlook.com>
2020-05-16 18:19:25 +0700

    unchanged

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -20,7 +20,7 @@ namespace ConsoleTest
             var arrs = new string[]{ "iy57g8u8","8b4jv0v1","253zi3b9","w60fjtg7","5pf4eci9",
                                      "u43g2g6g","5m69ax7h","3y46fa1w","0a5p3lv2","lwqb357p"};
             foreach (var arr in arrs)
-            { 
+            {                  
                 text = arr;
                 var s = ParseNumber();
                 Console.WriteLine($"{text} --> {s}");
@@ -69,6 +69,7 @@ namespace ConsoleTest
             }
             return -1;
         }
+
         private static char MoveNext()
         {
             var current = Peek(0);
