492e3fd19aded22db6b33874739104526541561d
Atip Chananantachot <47235392+Chananantachot@users.noreply.github.com>
2020-05-21 06:56:34 +0700

    Add more test case

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleTest
         static void Test()
         {
             var arrs = new string[]{ "iy57g8u8","8b4jv0v1","253zi3b9","w60fjtg7","5pf4eci9",
-                                     "u43g2g6g","5m69ax7h","3y46fa1w","0a5p3lv2","lwqb357p" };
+                                     "u43g2g6g","5m69ax7h","3y46fa1w","0a5p3lv2","lwqb357p","0a" };
             foreach (var arr in arrs)
             {                  
                 text = arr;
